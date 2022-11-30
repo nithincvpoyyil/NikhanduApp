3aa91aa4d737e3f56c9f79e619c24370070efa12
nithin <nithincv1@gmail.com>
2022-11-30 16:28:20 +0530

    Added new DB path

--- app/InfoScreen.tsx
+++ app/InfoScreen.tsx
@@ -10,20 +10,12 @@ import {
   CloseIcon,
   IconButton,
   Link,
-  HStack,
 } from 'native-base';
 import {InterfaceVStackProps} from 'native-base/lib/typescript/components/primitives/Stack/VStack';
 import AnimatedSlideUp from './components/AnimatedSlideUp';
 import {DeviceLightMode} from './types';
-import LightMode from './components/LightMode';
 import TextAnimator from './components/TextAnimator';
-import {
-  enText,
-  links,
-  malayalamtext1,
-  malayalamText2,
-} from './utils/textConstants';
-
+import {enText, links, malayalamtext1} from './utils/textConstants';
 export const vStackProps: InterfaceVStackProps = {
   space: 5,
   alignItems: 'center',
@@ -34,7 +26,6 @@ const animatedSlideUpProps = {style: {width: '100%', height: '100%'}};
 
 export default function InfoScreen({
   onPressCloseBtn,
-  changeAppLightMode,
 }: {
   onPressCloseBtn: () => void;
   changeAppLightMode: (mode: DeviceLightMode) => void;
@@ -43,6 +34,7 @@ export default function InfoScreen({
   const [animDirection, setAnimDirection] = React.useState<
     'forward' | 'reverse'
   >('forward');
+
   const handleOnPress = () => {
     setAnimDirection('reverse');
     setIsCloseClicked(true);
@@ -53,6 +45,7 @@ export default function InfoScreen({
       onPressCloseBtn();
     }
   };
+
   return (
     <Center bg="#ffa">
       <AnimatedSlideUp
@@ -88,25 +81,9 @@ export default function InfoScreen({
         <Flex flexGrow={1} width={'100%'} pl={'5%'} pr={'5%'}>
           <ScrollView horizontal={false} width="100%">
             <VStack {...vStackProps}>
-              {/* <LightMode
-                  currentMode="device"
-                  changeAppLightMode={changeAppLightMode}
-                /> */}
               <Box mb={1} mt={5} borderWidth={2} pt={2} pl={4} pr={4} pb={2}>
                 <Text fontSize="2xl">About</Text>
               </Box>
-
-              <TextAnimator
-                content={malayalamText2}
-                duration={800}
-                // eslint-disable-next-line react-native/no-inline-styles
-                textStyle={{
-                  fontSize: 16,
-                  paddingTop: 8,
-                }}
-                // eslint-disable-next-line react-native/no-inline-styles
-                wrapperStyle={{justifyContent: 'flex-start'}}
-              />
 
               <TextAnimator
                 content={malayalamtext1}
