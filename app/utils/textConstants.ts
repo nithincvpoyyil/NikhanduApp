f556a1c7f63ea161f72c67511915230604266564
nithin <nithincv1@gmail.com>
2023-03-19 13:21:45 +1100

    Added build chnages

--- app/utils/textConstants.ts
+++ app/utils/textConstants.ts
@@ -1,15 +1,15 @@
 const malayalamtext1 =
   'Olam - വളരുന്ന, സ്വതന്ത്രവും തുറന്നതുമായ, രണ്ടു ലക്ഷത്തിൽപ്പരം രേഖകളുള്ള ഒരു ഇംഗ്ലീഷ്-മലയാളം നിഘണ്ടു സമുച്ചയം.';
 const enText =
-  "Nikhandu app is based on Olam's English-Malayalam dictionary dataset with over 100K+ Malayalam definitions for more than 50k+ English words. Please refer the links below for more information.";
+  "Nikhandu app is built on Olam's English-Malayalam dictionary dataset with over 100K+ Malayalam definitions for more than 50k+ English words. Please refer to the links below for more information.";
 
 const analyticsDeclaration =
-  'Our analytics code is only collecting information related to word searches & failures for improving the app. If you are not happy with this, please turn it off.';
+  "Don't panic! We're just collecting searched words and failures using analytics. Through this, we will get insights into search queries and helps to improve the app. If you are not happy with this, please turn it off.";
 
 const links = [
   {key: 'Olam Dataset', link: 'https://olam.in/open/enml/'},
   {
-    key: 'Author & Source code',
+    key: 'Contributers',
     link: 'https://github.com/nithincvpoyyil/nikhanduapp',
   },
 ];
